76823faa955d4fc0f63cdf7165d865a7e1e9af63
Xianrui Yin <xianrui.yin@tum.de>
2023-11-11 22:17:08 +0100

    set environment variable for multithreading

--- hellomps/__init__.py
+++ hellomps/__init__.py
@@ -2,3 +2,9 @@
 # -*- coding: utf-8 -*-
 
 __author__='Xianrui Yin'
+
+import os
+
+os.environ["OMP_NUM_THREADS"] = "4"
+os.environ["NUMEXPR_NUM_THREADS"] = "4"
+os.environ["OPENBLAS_NUM_THREADS"] = "4"
