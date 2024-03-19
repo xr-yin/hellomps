e2952db9c44ccbaa76901200ea2e7dc748c08837
Xianrui Yin <xianrui.yin@tum.de>
2024-03-19 13:55:19 +0100

    initiate multithreading

--- hellomps/__init__.py
+++ hellomps/__init__.py
@@ -5,6 +5,8 @@ __author__='Xianrui Yin'
 
 import os
 
-os.environ["OMP_NUM_THREADS"] = "4"
-os.environ["NUMEXPR_NUM_THREADS"] = "4"
-os.environ["OPENBLAS_NUM_THREADS"] = "4"
+cpu_deploy = input('number of threads to use:') or '4'
+
+os.environ["OMP_NUM_THREADS"] = cpu_deploy
+os.environ["NUMEXPR_NUM_THREADS"] = cpu_deploy
+os.environ["OPENBLAS_NUM_THREADS"] = cpu_deploy
